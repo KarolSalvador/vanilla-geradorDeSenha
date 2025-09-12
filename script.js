a56595d32dc28384365fe41248754da1697a9616
KarolSalvador <karolsalvador91@gmail.com>
2025-09-12 10:02:12 -0300

    envio inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+const normalBtn = document.getElementById("normal-btn");
+const prioritarioeBtn = document.getElementById("prioritario-btn");
+
+normalBtn.addEventListener("click", () => {
+  console.log("pegou o botão normal");
+});
+
+prioritarioeBtn.addEventListener("click", () => {
+  console.log("pegou o botão prioridade");
+});
