828838b5b3781686c109350ecbad2b8afef80eb3
KarolSalvador <karolsalvador91@gmail.com>
2025-09-12 16:04:41 -0300

    adiciona ação aos botoes e salva selecao no localStorage

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const normalBtn = document.getElementById("normal-btn");
-const prioritarioeBtn = document.getElementById("prioritario-btn");
+const prioritarioBtn = document.getElementById("prioritario-btn");
+const voltarBtn = document.getElementById("voltar-btn");
 
-normalBtn.addEventListener("click", () => {
-  console.log("pegou o botão normal");
-});
+if (voltarBtn) {
+  voltarBtn.addEventListener("click", () => {
+    window.location.href = "index.html";
+  });
+}
 
-prioritarioeBtn.addEventListener("click", () => {
-  console.log("pegou o botão prioridade");
-});
+if (normalBtn) {
+  normalBtn.addEventListener("click", () => {
+    localStorage.setItem("tipoAtendimento", "normal");
+    window.location.href = "especialidade.html";
+  });
+}
+
+if (prioritarioBtn) {
+  prioritarioBtn.addEventListener("click", () => {
+    localStorage.setItem("tipoAtendimento", "prioritario");
+    window.location.href = "especialidade.html";
+  });
+}
