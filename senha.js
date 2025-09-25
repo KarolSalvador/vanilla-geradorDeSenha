aa85fa9cff6dd544c34ec90ecfd62136bd2a2c36
KarolSalvador <karolsalvador91@gmail.com>
2025-09-25 09:37:19 -0300

    Exibe senha real da API na tela final e adiciona limpeza do localStorage

--- senha.js
+++ senha.js
@@ -1,20 +1,29 @@
+//PEgaos dados salvos no localStorage
 const tipoAtendimento = localStorage.getItem("tipoAtendimento");
 const especialidade = localStorage.getItem("especialidade");
+const senhaGerada = localStorage.getItem("senhaGerada");
 
+//Encontrar elementos HTML na página para atualizar
 const tipoAtendimentoSpan = document.getElementById("tipo-atendimento");
 const especialidadeSpan = document.getElementById("especialidade");
+const numeroSenhaH2 = document.getElementById("numero-senha");
 
+//atualiza o conteúdo dos elementos com os dados gerados
 tipoAtendimentoSpan.textContent = tipoAtendimento;
 especialidadeSpan.textContent = especialidade;
 
-const numeroSenha = "Ate01";
-const numeroSenhaH2 = document.getElementById("numero-senha");
-numeroSenhaH2.textContent = `Senha: ${numeroSenha}`;
+//exibe a senha gerada pelo backend
+numeroSenhaH2.textContent = `Senha: ${senhaGerada}`;
 
 const voltarBtn = document.getElementById("voltar-btn");
 
 if (voltarBtn) {
   voltarBtn.addEventListener("click", () => {
+    //limpa a senha do localStorage para que gerar nova senha
+    localStorage.removeItem("tipoAtendimento");
+    localStorage.removeItem("especialidade");
+    localStorage.removeItem("senhaGerada");
+
     window.location.href = "index.html";
   });
 }
