ef5526c9b9956ef6ba5b589b31a80e9049908b96
KarolSalvador <karolsalvador91@gmail.com>
2025-09-25 09:31:23 -0300

    feat: Integra a chamada à API de geração de senha e salva o resultado

--- especialidade.js
+++ especialidade.js
@@ -11,7 +11,37 @@ especialidadeBtns.forEach((button) => {
     const especialidade = button.textContent;
     localStorage.setItem("especialidade", especialidade);
 
-    window.location.href = "senha.html";
+    const dadosCompletos = {
+      tipo: tipoAtendimento,
+      especialidade: especialidade,
+    };
+
+    //Enviar dados da solicitação para o backend
+    fetch("http://localhost:5000/gerar-senha", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dadosCompletos),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        //recebe a resposta e salva a senha
+        if (data.senha) {
+          localStorage.setItem("senhaGerada", data.senha);
+          //redireciona para tela de senha
+          window.location.href = "senha.html";
+        } else {
+          console.error("Erro ao gerar senha:", data.erro);
+          alert("Não foi possível gerar a senha. Tente novamente.");
+        }
+      })
+      .catch((error) => {
+        console.error("Erro na comunicação com o servidor:", error);
+        alert(
+          "Erro de conexão. Verifique se o servidor Node.js está rodando na porta 5000."
+        );
+      });
   });
 });
 
